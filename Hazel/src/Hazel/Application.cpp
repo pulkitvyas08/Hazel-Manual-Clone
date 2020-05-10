235f1b21dd3a0d6aac2c6513d81ca267d29a54ee
pulkitvyas08 <e19cse284@bennett.edu.in>
2020-05-10 12:35:43 +0530

    abstraction done (kind of)

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -13,6 +13,27 @@ namespace Hazel {
 
 	Application* Application::s_Instance = nullptr;
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+		case ShaderDataType::Float:    return GL_FLOAT;
+		case ShaderDataType::Float2:   return GL_FLOAT;
+	 	case ShaderDataType::Float3:   return GL_FLOAT;
+		case ShaderDataType::Float4:   return GL_FLOAT;
+		case ShaderDataType::Mat3:     return GL_FLOAT;
+		case ShaderDataType::Mat4:     return GL_FLOAT;
+		case ShaderDataType::Int:      return GL_INT;
+		case ShaderDataType::Int2:     return GL_INT;
+		case ShaderDataType::Int3:     return GL_INT;
+		case ShaderDataType::Int4:     return GL_INT;
+		case ShaderDataType::Bool:     return GL_BOOL;
+		}
+
+		HZ_CORE_ASSERT(false, "Unknown ShaderDataType");
+		return 0;
+	}
+
 	Application::Application()
 	{
 		HZ_CORE_ASSERT(!s_Instance, "Application already exists!");
@@ -37,11 +58,20 @@ namespace Hazel {
 		
 		BufferLayout layout = {
 			{ ShaderDataType::Float3, "a_Position" }
-		}
-		m_VertexBuffer->SetLayout();
+		};
 
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+		uint32_t index = 0;
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(index, 
+				element.GetComponentCount(), 
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE, 
+				layout.GetStride(), 
+				(const void*)element.Offset);
+			index++;
+		}
 
 		unsigned int indices[4] = { 0, 1, 2 };
 		m_IndexBuffer.reset(IndexBuffer::Create(indices, sizeof(indices) / sizeof(uint32_t)));
--- Hazel/src/Hazel/Renderer/Buffer.h
+++ Hazel/src/Hazel/Renderer/Buffer.h
@@ -34,21 +34,53 @@ namespace Hazel {
 		ShaderDataType Type;
 		uint32_t Offset;
 		uint32_t Size;
+		bool Normalized;
 
-		BufferElement(ShaderDataType type, const std::string& name) : Name(name), Type(type), Size(ShaderDataTypeSize(type)), Offset(0)
+		BufferElement() {}
+
+		BufferElement(ShaderDataType type, const std::string& name, bool normalized = false) 
+			: Name(name), Type(type), Size(ShaderDataTypeSize(type)), Offset(0), Normalized(normalized)
 		{
+		}
+
+		uint32_t GetComponentCount() const
+		{
+			switch (Type)
+			{
+			case ShaderDataType::Float:    return 1;
+			case ShaderDataType::Float2:   return 2;
+			case ShaderDataType::Float3:   return 3;
+			case ShaderDataType::Float4:   return 4;
+			case ShaderDataType::Mat3:     return 3 * 3;
+			case ShaderDataType::Mat4:     return 4 * 4;
+			case ShaderDataType::Int:      return 1;
+			case ShaderDataType::Int2:     return 2;
+			case ShaderDataType::Int3:     return 3;
+			case ShaderDataType::Int4:     return 4;
+			case ShaderDataType::Bool:     return 1;
+			}
+
+			HZ_CORE_ASSERT(false, "Unknown ShaderDataType");
+			return 0;
 		}
 	};
 
 	class BufferLayout
 	{
 	public:
+		BufferLayout() {}
+
 		BufferLayout(const std::initializer_list<BufferElement>& elements) : m_Elements(elements) 
 		{
-			CalculateOffsetAndStride()
+			CalculateOffsetAndStride();
 		}
 
+		inline uint32_t GetStride() const { return m_Stride; }
 		inline const std::vector<BufferElement>& GetElements() const { return m_Elements; }
+
+		std::vector<BufferElement>::iterator begin() { return m_Elements.begin(); }
+		std::vector<BufferElement>::iterator end() { return m_Elements.end(); }
+
 	private:
 		void CalculateOffsetAndStride()
 		{
@@ -73,6 +105,9 @@ namespace Hazel {
 
 		virtual void Bind() const = 0;
 		virtual void Unbind() const = 0;
+
+		virtual void SetLayout(const BufferLayout& layout) = 0;
+		virtual const BufferLayout& GetLayout() const = 0;
 
 		static VertexBuffer* Create(float* vertices, uint32_t size);
 	};
