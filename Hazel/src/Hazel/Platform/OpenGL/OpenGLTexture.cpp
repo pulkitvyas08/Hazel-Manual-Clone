b774b112c5b0434f6f02017c799c93d06a322217
pulkitvyas08 <e19cse284@bennett.edu.in>
2020-05-18 16:04:29 +0530

    Removed triangle + added square + preparation for texture assets (halfway episode 42)

---
+++ Hazel/src/Hazel/Platform/OpenGL/OpenGLTexture.cpp
@@ -0,0 +1,21 @@
+#include "hzpch.h"
+#include "Hazel/Platform/OpenGL/OpenGLTexture.h"
+
+namespace Hazel {
+
+	OpenGLTexture2D::OpenGLTexture2D(const std::string& path) : m_Path(path)
+	{
+
+	}
+
+	OpenGLTexture2D::~OpenGLTexture2D()
+	{
+
+	}
+
+	void OpenGLTexture2D::Bind() const
+	{
+
+	}
+
+}
